434391a88e8abc4219b89f006e7892b5e48598cd
WataruMaeda <wm19850716@gmail.com>
2016-08-31 00:32:47 +0900

    テーブル実装

--- samplePhonebookApp-2/ViewController.swift
+++ samplePhonebookApp-2/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet var tv: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    // MARK: - UITableView
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // セルの数
+        return 20
+    }
+    
+    // セルを一つ一つ指定する
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        // 👇とりあえず書いときましょう
+        let identifer = "cell"
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: identifer)
+    
+        // セルのデザイン
+        cell.textLabel?.text = "初心者歓迎iOS Development Meetup!"
+        cell.detailTextLabel?.text = "000-0000-0000"
+        
+        return cell
+    }
+    
+    // MARK: - Support
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
